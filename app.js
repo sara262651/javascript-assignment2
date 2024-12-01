e799f4032e886c29368e4bbd29d2902ce5fb5e59
sara262651 <sarasajid225@gmail.com>
2024-12-01 08:15:20 -0800

    chap 21-25 via javascript

---
+++ app.js
@@ -0,0 +1,81 @@
+// ............Question no 01............
+let firstName = prompt("Enter your first name:");
+let lastName = prompt("Enter your last name:");
+let fullName = firstName + " " + lastName;
+alert(`Hello, ${fullName}! Nice to meet you!`);
+
+// ............Question no 02............
+// let favoritePhone = prompt("Enter your favorite mobile phone model:");
+// console.log(`My favorite phone is: ${favoritePhone}`);
+// console.log(`Length of string: ${favoritePhone.length}`);
+
+
+// ............Question no 03............
+// let word = "Pakistani";
+// let index = word.indexOf('n');
+// console.log(`String: ${word}`);
+// console.log(`Index of 'n': ${index}`);
+
+// ............Question no 04............
+// let word = "Hello World";
+// let lastIndex = word.lastIndexOf('l');
+// console.log(`String: ${word}`);
+// console.log(`Last index of 'l': ${lastIndex}`);
+
+
+// ............Question no 05............
+// let word = "Pakistani";
+// let charAtIndex = word.charAt(3);
+// console.log(`String: ${word}`);
+// console.log(`Character at index 3: ${charAtIndex}`);
+
+
+// ............Question no 06............
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+// let fullName = firstName.concat(" ", lastName);
+// alert(`Hello, ${fullName}! Nice to meet you!`);
+
+
+// ............Question no 07............
+// let city = "Hyderabad";
+// let updatedCity = city.replace("Hyder", "Islam");
+// console.log(`City: ${city}`);
+// console.log(`After replacement: ${updatedCity}`);
+
+
+// ............Question no 08............
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var updatedMessage = message.replace(/and/g, "&");
+// console.log(`Original Message: ${message}`);
+// console.log(`Updated Message: ${updatedMessage}`);
+
+
+// ............Question no 09............
+// let strValue = "472";
+// let numValue = Number(strValue);
+// console.log(`Value: ${strValue}`);
+// console.log(`Type: ${typeof strValue}`);
+
+// console.log(`Value: ${numValue}`);
+// console.log(`Type: ${typeof numValue}`);
+
+
+// ............Question no 10............
+// let userInput = prompt("Enter a word:");
+// let upperCaseInput = userInput.toUpperCase();
+// console.log(`User input: ${userInput}`);
+// console.log(`Upper case: ${upperCaseInput}`);
+
+
+// ............Question no 11............
+// let userInput = prompt("Enter a word:");
+// let titleCaseInput = userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+// console.log(`User input: ${userInput}`);
+// console.log(`Title case: ${titleCaseInput}`);
+
+// ............Question no 12............
+// var num = 35.36;
+// var result = num.toString().replace('.', '');
+// console.log(`Number: ${num}`);
+// console.log(`Result: ${result}`);
